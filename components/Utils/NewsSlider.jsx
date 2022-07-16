ed92ab60d9b4c410ba7caa99989a3fd21dc13e18
markazcity <pr@markazknowledgecity.com>
2022-07-16 14:27:03 +0530

    newsSlider from bacend

--- components/Utils/NewsSlider.jsx
+++ components/Utils/NewsSlider.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import newses from "@/lib/news";
+// import newses from "@/lib/news";
 import { ROOT_URL } from "@/inc/Const";
 import { motion } from "framer-motion";
 import { BiNews } from "react-icons/bi";
@@ -9,33 +9,23 @@ import { useEffect } from "react";
 import { MdArrowForwardIos } from "react-icons/md";
 import React, { useRef, useState } from "react";
 
-const getData = async () => {
-  const NEWS_URL = ROOT_URL + "news/news.php?type=list";
-  let newsData = await fetch(NEWS_URL).then((response) => response.json());
-  return newsData;
-};
-
-export async function getStaticProps() {
-  const res = await fetch(`${ROOT_URL}news/news.php?type=list`);
-  console.log(res);
-
-  const content = await res.json();
-  console.log(content);
-  return {
-    props: {
-      newsData: content,
-    },
-    revalidate: 300,
-  };
-}
-
 const NewsSlider = ({ show, newsData }) => {
-  console.log(newsData);
+  const DATA_URL = "https://api.markazcity.in/news/news.php?type=list";
+
   const router = useRouter();
   const [width, setWidth] = useState(0);
-  // const [newses, setNewses] = useState(null);
+  const [newses, setNewses] = useState(null);
   const carousel = useRef();
-  // let show = true;
+
+  const getData = async () => {
+    fetch(DATA_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "HasNews") {
+          setNewses(data.newses);
+        }
+      });
+  };
 
   useEffect(() => {
     // if (newsData != null) {
@@ -51,6 +41,7 @@ const NewsSlider = ({ show, newsData }) => {
     //     });
     //   }
     // }
+    getData();
     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
   }, []);
   console.log(newses);
@@ -76,45 +67,42 @@ const NewsSlider = ({ show, newsData }) => {
             dragConstraints={{ right: 0, left: -width }}
             className="inner-carousel flex gap-4"
           >
-            {newses
-              .slice(0)
-              .reverse()
-              .map((news, i) => (
-                <div key={i}>
-                  <motion.div
-                    className=" item min-h-[20rem] min-w-[18rem] flex flex-col "
-                    key={i}
-                  >
-                    <div className="flex flex-col w-[280px]">
-                      <Image
-                        // src={ROOT_URL + news?.thumb}
-                        src={news?.imgUrl}
-                        alt={news?.title}
-                        width="200px"
-                        height="180px"
-                        style={{
-                          objectFit: "cover",
-                        }}
-                        // layout="fill"
-                        className="pointer-events-none w-[200px] "
-                      />
-                      <span className="mt-5 text-gray-700">
-                        {news?.title.toUpperCase()}
-                      </span>
-                      <p className="text-[12px] flex  text-center items-center mt-6 relative bottom-0 ">
-                        <Link href={`/media/news/${news?.news_link}`}>
-                          <span className="hover:text-gray transition hover:text-yellow-500">
-                            <Link href={`/news/${news?.news_link}`}>
-                              <a>Read More </a>
-                            </Link>
-                          </span>
-                        </Link>{" "}
-                        {/* <MdArrowForwardIos size={"10px"} className="ml-1" /> */}
-                      </p>
-                    </div>
-                  </motion.div>
-                </div>
-              ))}
+            {newses?.map((news, i) => (
+              <div key={i}>
+                <motion.div
+                  className=" item min-h-[20rem] min-w-[18rem] flex flex-col "
+                  key={i}
+                >
+                  <div className="flex flex-col w-[280px]">
+                    <Image
+                      src={ROOT_URL + news?.thumb}
+                      // src={news?.imgUrl}
+                      alt={news?.title}
+                      width="200px"
+                      height="180px"
+                      style={{
+                        objectFit: "cover",
+                      }}
+                      // layout="fill"
+                      className="pointer-events-none w-[200px] "
+                    />
+                    <span className="mt-5 text-gray-700">
+                      {news?.title.toUpperCase()}
+                    </span>
+                    <p className="text-[12px] flex  text-center items-center mt-6 relative bottom-0 ">
+                      <Link href={`/media/news/${news?.news_link}`}>
+                        <span className="hover:text-gray transition hover:text-yellow-500">
+                          <Link href={`/news/${news?.news_link}`}>
+                            <a>Read More </a>
+                          </Link>
+                        </span>
+                      </Link>{" "}
+                      {/* <MdArrowForwardIos size={"10px"} className="ml-1" /> */}
+                    </p>
+                  </div>
+                </motion.div>
+              </div>
+            ))}
           </motion.div>
         </motion.div>
       </div>
@@ -123,3 +111,15 @@ const NewsSlider = ({ show, newsData }) => {
 };
 
 export default NewsSlider;
+
+export async function getStaticProps() {
+  const res = await fetch(`https://api.markazcity.in/news/news.php?type=list`);
+  const newsData = await res.json();
+  return {
+    props: {
+      newsData: newsData.data,
+    },
+    revalidate: 1,
+  };
+  console.log("hi");
+}
